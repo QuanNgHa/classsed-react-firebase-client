8d8f9bf069d946216a637af6c011280fe9b84198
QuanNgHa <quanngha.dev@gmail.com>
2020-03-26 22:10:13 +0800

    #14 - Get Started with React

---
+++ src/components/Scream.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+const styles = {
+    card: {
+        display: 'flex'
+    }
+}
+export class Scream extends Component {
+    render() {
+        //const classes = this.props.classes;
+        const { classes } = this.props
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(Scream);
