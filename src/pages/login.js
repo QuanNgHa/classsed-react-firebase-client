670803e32dab59bc67aa00d63e43f5c31b6522a9
QuanNgHa <quanngha.dev@gmail.com>
2020-03-28 20:59:43 +0800

    Classed #17- Signup & Auth State

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,36 +14,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-    form: {
-        textAlign: 'center'
-    },
-    image: {
-        margin: '5px auto 5px auto',
-        width: 200,
-
-    },
-    pageTitle: {
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        marginTop: 10,
-        position: 'relative'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 10,
-    },
-    progress: {
-        position: 'absolute'
-
-    }
-
-}
+const styles = (theme) => ({
+    ...theme.spreadThis
+})
 
 
 export class login extends Component {
@@ -69,6 +42,7 @@ export class login extends Component {
         axios.post('/login', userData)
             .then(res => {
                 console.log(res.data);
+                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
                 this.setState({
                     loading: false
                 })
---
+++ src/util/theme.js
@@ -0,0 +1,51 @@
+export default {
+    palette: {
+        primary: {
+            light: '#33c9dc',
+            main: '#00bcd4',
+            dark: '#008394',
+            contrastText: '#fff'
+        },
+        secondary: {
+            light: '#ff6333',
+            main: '#ff3d00',
+            dark: '#b22a00',
+            contrastText: '#fff'
+        }
+    },
+
+    spreadThis: {
+        typography: {
+            useNextVariants: true
+        },
+        form: {
+            textAlign: 'center'
+        },
+        image: {
+            margin: '5px auto 5px auto',
+            width: 200,
+
+        },
+        pageTitle: {
+            margin: '10px auto 10px auto'
+        },
+        textField: {
+            margin: '10px auto 10px auto'
+        },
+        button: {
+            marginTop: 10,
+            position: 'relative'
+        },
+        customError: {
+            color: 'red',
+            fontSize: '0.8rem',
+            marginTop: 10,
+        },
+        progress: {
+            position: 'absolute'
+        },
+
+
+    },
+
+}
